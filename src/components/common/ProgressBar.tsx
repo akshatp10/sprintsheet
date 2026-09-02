f2bc19f2ecaeeaa4b39fc4983a3191a002f5b780
akshatp10 <akshat@thealteroffice.com>
2026-09-02 11:07:02 +0530

    feat : passing additional props and implementing cn in global components for handling classname conflicts

--- src/components/common/ProgressBar.tsx
+++ src/components/common/ProgressBar.tsx
@@ -1,19 +1,22 @@
 import { cn } from "@/lib/cn";
 
-interface ProgressBarProps {
+interface ProgressBarProps extends React.HTMLAttributes<HTMLDivElement> {
     progress: number; //Percentage out of 100
     color?: string;
+    classname?: string;
 }
 
 const ProgressBar = ({
     progress,
     color = "bg-ink",
+    classname,
+    ...props
 }: ProgressBarProps) => {
 
     return (
         //Denoting total component/bg of the progressbar
         <div
-            className="bg-surface-track w-full h-2 rounded-full overflow-hidden">
+            className={cn("bg-surface-track w-full h-2 rounded-full overflow-hidden", classname)} {...props}>
             {/* Progress of progressbar */}
             <div
                 className={cn("h-full rounded-full transition-all duration-300 ease-out", color)}
--- src/components/identity/AvatarGroups.tsx
+++ src/components/identity/AvatarGroups.tsx
@@ -2,9 +2,9 @@ interface AvatarGroupProps {
     children: React.ReactNode;
 }
 
-const AvatarGroup = ({ children }: AvatarGroupProps) => {
+const AvatarGroup = ({ children, ...props }: AvatarGroupProps) => {
     return (
-        <div className="flex items-center -space-x-1.5 *:border *:border-white">
+        <div className="flex items-center -space-x-1.5 *:border *:border-white" {...props}>
             {children}
         </div>
     );
