3c9bda315dc13d7f2d6f62a2327661968f83d1d3
akshatp10 <akshat@thealteroffice.com>
2026-09-03 16:03:07 +0530

    feat : Modular Multistep Form setup for creation of new project

--- src/pages/AllProjectsPage.tsx
+++ src/pages/AllProjectsPage.tsx
@@ -1,6 +1,7 @@
 import Text from "@/components/common/Text";
 import Tabs, { type TabsOption } from "@/components/inputs/Tabs";
 import EmptyTaskCard from "@/features/projects/components/EmptyTaskCard";
+import NewProjectForm from "@/features/projects/components/forms/NewProjectForm";
 import ProjectCardGrid from "@/features/projects/components/ProjectCardGrid";
 import { useState } from "react";
 
