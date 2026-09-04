550d1e758b224476a1ae06451840f069af5ff554
akshatp10 <akshat@thealteroffice.com>
2026-09-04 11:25:21 +0530

    refactor : moved the project creation form inside the empty task card and also synced it with workspaceTopbar

--- src/pages/AllProjectsPage.tsx
+++ src/pages/AllProjectsPage.tsx
@@ -1,7 +1,6 @@
 import Text from "@/components/common/Text";
 import Tabs, { type TabsOption } from "@/components/inputs/Tabs";
 import EmptyTaskCard from "@/features/projects/components/EmptyTaskCard";
-import NewProjectForm from "@/features/projects/components/forms/NewProjectForm";
 import ProjectCardGrid from "@/features/projects/components/ProjectCardGrid";
 import { mapProjectToCard } from "@/lib/mapProjectToCard";
 import { getAllUserProjects, getProjectMembers } from "@/lib/services/api/projectServices";
