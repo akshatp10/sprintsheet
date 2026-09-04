550d301f9688386834904d963ddd7fd25c151b42
akshatp10 <akshat@thealteroffice.com>
2026-09-04 11:03:50 +0530

    feat : form working for project creation and remodelling the global components to work with form

--- src/features/projects/components/forms/NewProjectForm.tsx
+++ src/features/projects/components/forms/NewProjectForm.tsx
@@ -14,6 +14,26 @@ interface NewProjectFormProps {
     onClose: () => void
 }
 
+const defaultValues = {
+    name: "",
+    key: "",
+    description: "",
+    cycleLength: "default",
+    customCycleDays: "5",
+    startingDay: "",
+    autoCycle: false,
+    defaultView: "table",
+    stages: [
+        { name: "Backlog" },
+        { name: "Todo" },
+        { name: "In progress" },
+        { name: "In QA" },
+        { name: "Done" },
+    ],
+    people: [],
+    startFirstCycle: true,
+}
+
 const NewProjectForm = ({ onClose }: NewProjectFormProps) => {
 
     const { mutate: createProject } = useCreateProject()
