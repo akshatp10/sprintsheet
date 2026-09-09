504ee26c50dc520831b92410fa7b276c97314cb4
akshatp10 <akshat@thealteroffice.com>
2026-09-09 16:55:47 +0530

    fix : removed unused variant from SidebarBottom which blocked rendering

--- src/components/sidebar/SidebarBottom.tsx
+++ src/components/sidebar/SidebarBottom.tsx
@@ -8,7 +8,7 @@ const SidebarBottom = () => {
             <SettingsButton isAdmin />
             <hr className="text-lines-hairline" />
             <div className="flex w-full justify-between items-center">
-                <UserPanel userName="Akshat Pratyush" isSidebar variant="blue" textVariant="h2" textColor="text-ink-2" />
+                <UserPanel userName="Akshat Pratyush" isSidebar textVariant="h2" textColor="text-ink-2" />
                 <ExtraOptionButton handleClick={() => { }} />
             </div>
         </div>
