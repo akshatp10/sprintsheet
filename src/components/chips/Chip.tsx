95cd7e09e12f003cb3f90b1ae660e6a978f76ccc
akshatp10 <akshat@thealteroffice.com>
2026-09-09 14:59:57 +0530

    style : added default styling to chip

--- src/components/chips/Chip.tsx
+++ src/components/chips/Chip.tsx
@@ -2,20 +2,21 @@ import { cn } from '@/lib/cn'
 
 interface ChipProps extends React.HTMLAttributes<HTMLDivElement> {
     text: string
-    variant: "primary" | "secondary"
+    variant?: "primary" | "secondary" | "default"
     bgColor?: string
     borderColor?: string
     textColor?: string
     textType?: string
 }
 
-const Chip = ({ text, variant, bgColor = "bg-transparent", borderColor = "border border-lines-control", textColor = "text-ink-2", textType = "text-type-body-sm", ...props }: ChipProps) => {
+const Chip = ({ text, variant = "default", bgColor = "", borderColor = "", textColor = "", textType = "", ...props }: ChipProps) => {
 
     const commonClass = "w-fit h-fit text-center px-2 py-0.5 text-type-body rounded-sm text-ink-2";
 
     const variantClasses = {
-        primary: commonClass + " bg-lines-control",
-        secondary: commonClass + " bg-transparent border border-lines-border"
+        primary: cn(commonClass, " bg-lines-control"),
+        secondary: cn(commonClass, " bg-transparent border border-lines-border"),
+        default: cn(commonClass, "bg-transparent border border-lines-border"),
     }
 
     return (
