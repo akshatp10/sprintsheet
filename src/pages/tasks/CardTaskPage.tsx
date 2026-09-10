e7b93f59cc870d9a88896f6bdc9a242fff500445
akshatp10 <akshat@thealteroffice.com>
2026-09-10 17:32:02 +0530

    feat : working card view layout with colours

--- src/pages/tasks/CardTaskPage.tsx
+++ src/pages/tasks/CardTaskPage.tsx
@@ -1,4 +1,5 @@
 import TaskCard from "@/features/tasks/components/TaskCard";
+import { cn } from "@/lib/cn";
 import type { Stage } from "@/lib/services/stages/type";
 import type { Task } from "@/lib/services/tasks/types";
 
@@ -15,6 +16,15 @@ const CardTaskPage = ({ tasks, stages, isLoading }: CardTaskPageProps) => {
     const sortedStages = [...stages].sort((a, b) => a.order - b.order);
     console.log(sortedStages);
 
+    const stageColors: Record<string, string> = {
+        Backlog: "bg-stage-backlog-bg border border-stage-blocked-border",
+        Todo: "bg-stage-todo-bg border border-stage-todo-border",
+        "In progress": "bg-stage-progress-bg border border-stage-progress-border",
+        "In QA": "bg-stage-qa-bg border border-stage-qa-border",
+        Done: "bg-stage-done-bg border border-stage-done-border",
+        Blocked: "bg-stage-blocked-bg border border-stage-blocked-border"
+    };
+
     return (
         <div
             className="grid gap-4 overflow-x-auto p-4 h-full"
@@ -23,14 +33,15 @@ const CardTaskPage = ({ tasks, stages, isLoading }: CardTaskPageProps) => {
             }}
         >
             {sortedStages.map((stage) => {
+                //TODO - Optimize the task filtering
                 const stageTasks = tasks.filter((t) => t.stageId === stage.stageId);
 
                 return (
                     <div
                         key={stage.id}
-                        className="flex h-full flex-col rounded-md bg-muted/50"
+                        className={cn("flex h-full flex-col rounded-md", stageColors[stage.name])}
                     >
-                        <div className="flex shrink-0 items-center justify-between border-b p-4">
+                        <div className="flex shrink-0 items-center justify-between px-4 pt-2">
                             <h2 className="font-semibold">{stage.name}</h2>
                             <span className="text-sm text-muted-foreground">
                                 {stageTasks.length}
@@ -47,7 +58,7 @@ const CardTaskPage = ({ tasks, stages, isLoading }: CardTaskPageProps) => {
                                     />
                                 ))
                             ) : (
-                                <div className="flex h-24 items-center justify-center rounded-md border border-dashed text-sm text-muted-foreground">
+                                <div className="flex h-24 items-center justify-center rounded-md border border-dashed border-lines-control text-sm text-ink-2">
                                     No tasks
                                 </div>
                             )}
