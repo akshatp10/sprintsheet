1b1692b043db389bffa7c5eb206e957340e84db3
akshatp10 <akshat@thealteroffice.com>
2026-09-10 18:29:16 +0530

    refactor : renamed slideover to drawer

--- src/components/drawer/DrawerHeader.tsx
+++ src/components/drawer/DrawerHeader.tsx
@@ -2,15 +2,15 @@ import Button from "@/components/button/Button";
 import Text from "@/components/common/Text";
 import { X } from "lucide-react";
 
-interface SlideOverHeaderProps {
+interface DrawerHeaderProps {
     onClose: () => void;
     label?: string;
 }
 
-const SlideOverHeader = ({
+const DrawerHeader = ({
     onClose,
     label = "",
-}: SlideOverHeaderProps) => {
+}: DrawerHeaderProps) => {
     return (
         <div className="flex items-center justify-between p-4 border-b border-lines-hairline">
             <Text variant="h1">
@@ -29,4 +29,4 @@ const SlideOverHeader = ({
     );
 };
 
-export default SlideOverHeader;
+export default DrawerHeader;
