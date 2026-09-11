d909e76b89cc2f3e6e5a81328a2bc877ec079a6f
akshatp10 <akshat@thealteroffice.com>
2026-09-11 15:04:42 +0530

    feat : removed backlog from card view and had the parent be the SOT for tasks

--- src/pages/TaskViewPage.tsx
+++ src/pages/TaskViewPage.tsx
@@ -4,6 +4,8 @@ import CardTaskPage from "./tasks/CardTaskPage";
 import { useTasks } from "@/lib/services/tasks/hooks";
 import { useProjectStages } from "@/lib/services/stages/hooks";
 import TaskViewFooter from "@/features/tasks/components/TaskViewFooter";
+import { useMemo } from "react";
+import type { Task } from "@/lib/services/tasks/types";
 
 const TaskViewPage = () => {
     const [searchParams] = useSearchParams();
@@ -14,15 +16,35 @@ const TaskViewPage = () => {
     const { data: tasks, isLoading, isError, error } = useTasks(projectid ?? "");
     const { data: stages = [] } = useProjectStages(projectid ?? "");
 
+    const taskList = tasks ?? [];
+
+    const sortedStages = [...stages].sort((a, b) => a.order - b.order);
+
+    const tasksByStage = useMemo(() =>
+        taskList.reduce<Record<string, Task[]>>((acc, task) => {
+            if (!acc[task.stageId])
+                acc[task.stageId] = [];
+
+            acc[task.stageId].push(task);
+            return acc;
+        }, {})
+        , [taskList]);
+
+    const backlogStage = sortedStages.find(
+        (stage) => stage.name === "Backlog"
+    );
+
+    const backlogTasks = tasksByStage[backlogStage?.stageId ?? ""] ?? [];
+
     if (isError) return (<div>Error : {error.message}</div>)
 
     return (
         <div className="grid h-full min-h-0 min-w-fit grid-rows-[1fr_2rem]">
             {view === "table" && (<TableTaskPage />)}
 
-            {view === "cards" && (<CardTaskPage tasks={tasks} stages={stages} isLoading={isLoading} projectId={projectid ?? ""} />)}
+            {view === "cards" && (<CardTaskPage stages={sortedStages} tasksByStage={tasksByStage} isLoading={isLoading} projectId={projectid ?? ""} />)}
 
-            <TaskViewFooter />
+            <TaskViewFooter taskLength={backlogTasks.length} />
         </div>
     );
 };
