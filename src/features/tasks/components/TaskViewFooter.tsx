6b8144a68a0d7e53bb827f8da865594691d9b083
akshatp10 <akshat@thealteroffice.com>
2026-09-17 13:12:18 +0530

    feat : implemented cycle creation

--- src/features/tasks/components/TaskViewFooter.tsx
+++ src/features/tasks/components/TaskViewFooter.tsx
@@ -1,37 +1,55 @@
 import Button from "@/components/button/Button"
 import Text from "@/components/common/Text"
+import CreateCycleForm from "@/features/cycles/forms/CreateCycleForm"
+import { useCycles } from "@/lib/services/cycles/hooks"
 import { Inbox, Menu, Plus } from "lucide-react"
+import { useState } from "react"
 
 interface TaskViewFooterProps {
     isCardHeld?: boolean
     taskLength?: number
     onClick: () => void
+    projectId: string;
 }
 
-const TaskViewFooter = ({ onClick, isCardHeld = true, taskLength = 0 }: TaskViewFooterProps) => {
-    return (
-        <div className='bg-surface-sunken border-t border-t-lines-hairline flex justify-between items-center px-4 z-20'>
-            <div className="flex">
-                {/* This is creation of cycle */}
-                <Button variant="tertiary" className="border-none p-1.5">
-                    <Plus strokeWidth={1.5} size={15} />
-                </Button>
-                {/* This is menu*/}
-                <Button variant="tertiary" className="border-none p-1.5">
-                    <Menu strokeWidth={1.5} size={15} />
-                </Button>
+const TaskViewFooter = ({ projectId, onClick, isCardHeld = true, taskLength = 0 }: TaskViewFooterProps) => {
 
-                {/* TODO : Add Cycle Tabs */}
+    const [createCycle, setCreateCycle] = useState(false)
+
+    const { data: cycles } = useCycles(projectId);
+
+    return (
+        <>
+            <div className='bg-surface-sunken border-t border-t-lines-hairline flex justify-between items-center px-4 z-20'>
+                <div className="flex">
+                    {/* This is creation of cycle */}
+                    <Button variant="tertiary" className="border-none p-1.5" onClick={() => { setCreateCycle(true) }}>
+                        <Plus strokeWidth={1.5} size={15} />
+                    </Button>
+                    {/* This is menu*/}
+                    <Button variant="tertiary" className="border-none p-1.5">
+                        <Menu strokeWidth={1.5} size={15} />
+                    </Button>
+
+                    {/* TODO : Add Cycle Tabs */}
+                    {cycles?.map((cycle) => (
+                        <>
+                            {cycle.name}
+                        </>
+                    ))}
+                </div>
+                <div className="flex gap-3 items-center">
+                    {isCardHeld && <Text variant="body-sm" className="text-ink-3">Card held - drop it in any stage column</Text>}
+                    <Button variant="tertiary" className="flex gap-1 items-center justify-center" onClick={onClick}>
+                        <Inbox strokeWidth={1.5} size={13} />
+                        <Text className="text-ink-2">Backlog</Text>
+                        <Text variant="body-sm" className="font-normal text-ink-3">{taskLength}</Text>
+                    </Button>
+                </div>
             </div>
-            <div className="flex gap-3 items-center">
-                {isCardHeld && <Text variant="body-sm" className="text-ink-3">Card held - drop it in any stage column</Text>}
-                <Button variant="tertiary" className="flex gap-1 items-center justify-center" onClick={onClick}>
-                    <Inbox strokeWidth={1.5} size={13} />
-                    <Text className="text-ink-2">Backlog</Text>
-                    <Text variant="body-sm" className="font-normal text-ink-3">{taskLength}</Text>
-                </Button>
-            </div>
-        </div>
+
+            {createCycle && <CreateCycleForm projectId={projectId} onClose={() => { setCreateCycle(false) }} />}
+        </>
     )
 }
 
